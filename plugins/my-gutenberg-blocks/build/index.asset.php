23d3310e259f7402b4a88fccebb2590f38d99e64
Jose Miguel <ramirez074jose@gmail.com>
2024-07-01 01:36:10 -0600

    Updated

---
+++ plugins/my-gutenberg-blocks/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '04e0b959542584182bd1');
---
+++ plugins/my-gutenberg-blocks/my-gutenberg-bloks.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: My Gutenberg Blocks
+ * Description: Mi plugin con bloques
+ * Version: 1.0.0
+ * Author: Edwin Delgado
+ */
+
+if (!defined('ABSPATH'))
+    exit;
+
+require_once plugin_dir_path(__FILE__) . 'includes/register-blocks.php';
